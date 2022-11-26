85023c32537e38416ea5d1b99f09cd7e2b21540c
jaazielirc <jaaziel.irc@gmail.com>
2022-11-26 09:40:37 -0600

    Test completados

--- app/src/test/java/com/jaax/dagger2loginexample/PresenterUnitTest.java
+++ app/src/test/java/com/jaax/dagger2loginexample/PresenterUnitTest.java
@@ -4,19 +4,14 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import com.jaax.dagger2loginexample.data.model.User;
 import com.jaax.dagger2loginexample.login.LoginActivityMVP;
 import com.jaax.dagger2loginexample.login.LoginActivityPresenter;
-import com.jaax.dagger2loginexample.login.LoginRepository;
-import com.jaax.dagger2loginexample.login.MemoryRepository;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 
 public class PresenterUnitTest {
 
@@ -64,5 +59,56 @@ public class PresenterUnitTest {
         verify(view, times(1)).setFirstName("jaax");
         verify(view, times(1)).setLastName("dev");
         verify(view, never()).showUserNotAvailable();
+    }
+
+    @Test
+    public void showErrorMessageWhenUserIsNull() {
+        when(model.getUser()).thenReturn(null);
+
+        presenter.getCurrentUser();
+
+        //comprobamos la interacción con el model de datos
+        verify(model, times(1)).getUser();
+
+        //comprobamos la interacción con la vista
+        verify(view, never()).setFirstName("jaax");
+        verify(view, never()).setLastName("dev");
+        verify(view, times(1)).showUserNotAvailable();
+    }
+
+
+    public void createErrorMessageIfAnyIsEmpty() {
+        //primera prueba poniendo firstName vacío
+        when(view.getFirstName()).thenReturn("");
+
+        presenter.loginButtonClicked();
+
+        verify(view, times(1)).getFirstName();
+        verify(view, never()).getLastName();
+        verify(view, times(1)).showInputError();
+
+        //segunda prueba con el campo lastname name vacío
+        when(view.getFirstName()).thenReturn("jaax");
+        when(view.getLastName()).thenReturn("");
+
+        presenter.loginButtonClicked();
+
+        verify(view, times(2)).getFirstName();
+        verify(view, times(1)).getLastName();
+        verify(view, times(2)).showInputError();
+    }
+
+    @Test
+    public void saveValidUser() {
+        when(view.getFirstName()).thenReturn("jaax");
+        when(view.getLastName()).thenReturn("dev");
+
+        presenter.loginButtonClicked();
+
+        verify(view, times(2)).getFirstName();
+        verify(view, times(1)).getLastName();
+
+        verify(model, times(1)).createUser("jaax", "dev");
+        verify(view, times(1)).showUserSaved();
     }
 }
